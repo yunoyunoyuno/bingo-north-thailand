bc81faf3dd0553307b7f751535441d7538a0aac4
yn <Moreseus@outlook.com>
2020-09-25 10:50:03 +0700

    Updates

---
+++ precache-manifest.009a2f52506d3193c305839af59bc064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0050a2bb0383afdd9e0689ab5b0859eb",
+    "url": "/bingo-north-thailand/index.html"
+  },
+  {
+    "revision": "6ce16199ed689909d2e9",
+    "url": "/bingo-north-thailand/static/css/main.41d43d26.chunk.css"
+  },
+  {
+    "revision": "5b938467a7a757761c27",
+    "url": "/bingo-north-thailand/static/js/2.5a8bc873.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo-north-thailand/static/js/2.5a8bc873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce16199ed689909d2e9",
+    "url": "/bingo-north-thailand/static/js/main.ed3bf85d.chunk.js"
+  },
+  {
+    "revision": "3a8a8992e14ba66919b9",
+    "url": "/bingo-north-thailand/static/js/runtime-main.7c2025f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bingo-north-thailand/static/media/logo.5d5d9eef.svg"
+  }
+]);
